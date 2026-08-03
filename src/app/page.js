ba48d4f7a427070901e79559af791e5fd8849217
Mehran Ali <mrmehranlala@gmail.com>
2026-08-03 21:04:07 +0500

    Add my Next.js project

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+   
+        <><h1 className="text-3x-l font-bold text-center">Welcome to Home js</h1>
+        <p className="text-center">This is the home page of the Next.js application.</p>
+        </>
+        
+  );
+}
