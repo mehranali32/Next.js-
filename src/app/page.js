3c223795237dc34414f26dadb028962b82b85b60
Mehran Ali <mrmehranlala@gmail.com>
2026-08-03 21:09:33 +0500

    my masseg

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-   
-        <><h1 className="text-3x-l font-bold text-center">Welcome to Home js</h1>
-        <p className="text-center">This is the home page of the Next.js application.</p>
-        </>
-        
+    <>
+      <h1 className="text-3x-l font-bold text-center">Welcome to Home js</h1>
+      <p className="text-center bg-amber-400 text-bold">
+        This is the home page of the Next.js application.
+      </p>
+      <h2 className="text-2xl font-semibold text-center mt-4">
+        Explore the features and enjoy your stay!
+      </h2>
+    </>
   );
 }
