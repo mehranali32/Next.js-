559ed2a9da4da390e30b16cb9cc227c3782aadb7
Mehran Ali <mrmehranlala@gmail.com>
2026-08-04 21:05:22 +0500

    Add my all files in which prsent in this time
    This is react project in which i use next.js,taiwlind css and some other defindencess

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import { BannerSec } from "@/components/common/HomeComponents/BannerSec";
+
+export default function Home() {
+  return (
+    <>
+    <BannerSec />
+    </>
+  );
+}
---
+++ src/components/common/HomeComponents/BannerSec.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "Banner.jpg";
+export const BannerSec = () => {
+    return (
+        <>
+            <div className="max-w-7xl mx-auto h-full flex flex-col items-center justify-center gap-6">
+            <section className='bg-[url("Banner.jpg")] bg-cover bg-centerh-screen w-full flex items-center justify-center'>
+                
+            </section>
+            <p className="text-white text-4xl font-bold">Welcome to Our Website Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptate facilis odit iste. Ullam iure illo fuga, nisi cumque vitae deserunt. Rerum tempora libero in laudantium amet facere? Dolorem, quisquam!
+                    Molestiae excepturi quibusdam corporis, mollitia sit harum blanditiis dolor recusandae. Iste amet animi officia enim, consequuntur quibusdam dolor voluptatibus distinctio sit optio, rerum consectetur maxime nam consequatur sequi voluptates accusamus.
+                    Explicabo aut amet deleniti illum voluptate, quam quae tempora ipsa delectus atque minus, assumenda natus error, sed illo nam. Voluptatibus officia quae blanditiis perferendis veritatis repellat saepe assumenda cumque esse.</p>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">Get Started</button>
+        </div>
+        </>
+    );
+};
